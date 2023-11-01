864a4c758fd338e89826b2d17e5384d64f8092ce
VigersRay <vigersray@gmail.com>
2023-11-01 23:12:15 +0300

    fix ban webhook

--- Content.Shared/AdventureSpace/CCVars.cs
+++ Content.Shared/AdventureSpace/CCVars.cs
@@ -1,11 +1,15 @@
-namespace Content.Shared.CCVar
+using Robust.Shared;
+using Robust.Shared.Configuration;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Andromeda.CCVar
 {
     // ReSharper disable once InconsistentNaming
     [CVarDefs]
-    public sealed class CCVars : CVars
+    public sealed class AndromedaCCVars : CVars
     {
         /// <summary>
-        /// URL of the Discord webhook which will relay all ban messages.
+        /// URL of the Discord webhook which will show bans in game.
         /// </summary>
         public static readonly CVarDef<string> DiscordBanWebhook =
             CVarDef.Create("discord.ban_webhook", string.Empty, CVar.SERVERONLY);
