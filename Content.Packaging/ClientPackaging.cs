1f6af2185e046c547abceeaefe368c8f9b29131b
TAZIKLIK <73418250+TAZIKLIK@users.noreply.github.com>
2023-11-09 01:24:01 +0300

    Update ClientPackaging.cs

--- Content.Packaging/ClientPackaging.cs
+++ Content.Packaging/ClientPackaging.cs
@@ -69,7 +69,7 @@ public static class ClientPackaging
             inputPass,
             contentDir,
             "Content.Client",
-            new[] { "Content.Client", "Content.Shared", "Content.Shared.Database" },
+            new[] { "Content.Client", "Content.Shared", "Content.Shared.Database", "Content.Corvax.Interfaces.Shared", "Content.Corvax.Interfaces.Client", "Content.Corvax.Shared", "Content.Corvax.Client" },
             cancel: cancel);
 
         await RobustClientPackaging.WriteClientResources(contentDir, pass, cancel);
