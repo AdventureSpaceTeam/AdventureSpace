space-wizards/space-station-14

commit 9f47079d02a872f0aaff84f4556b83d3c6f835ef

Fix remote solution injection (#23429)
Checks if its own fixture is hard so the fly-by fixture can't also proc it.

Content.Server/Chemistry/Components/SolutionInjectOnCollideComponent.cs
@@ -1,29 +1,27 @@
 ﻿using Content.Shared.FixedPoint;
 using Content.Shared.Inventory;
 
-namespace Content.Server.Chemistry.Components
+namespace Content.Server.Chemistry.Components;
+
+/// <summary>
+/// On colliding with an entity that has a bloodstream will dump its solution onto them.
+/// </summary>
+[RegisterComponent]
+public sealed partial class SolutionInjectOnCollideComponent : Component
 {
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("transferAmount")]
+    public FixedPoint2 TransferAmount = FixedPoint2.New(1);
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float TransferEfficiency { get => _transferEfficiency; set => _transferEfficiency = Math.Clamp(value, 0, 1); }
+
+    [DataField("transferEfficiency")]
+    private float _transferEfficiency = 1f;
+
     /// <summary>
-    /// On colliding with an entity that has a bloodstream will dump its solution onto them.
+    /// If anything covers any of these slots then the injection fails.
     /// </summary>
-    [RegisterComponent]
-    internal sealed partial class SolutionInjectOnCollideComponent : Component
-    {
-
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField("transferAmount")]
-        public FixedPoint2 TransferAmount { get; set; } = FixedPoint2.New(1);
-
-        [ViewVariables(VVAccess.ReadWrite)]
-        public float TransferEfficiency { get => _transferEfficiency; set => _transferEfficiency = Math.Clamp(value, 0, 1); }
-
-        [DataField("transferEfficiency")]
-        private float _transferEfficiency = 1f;
-
-        /// <summary>
-        /// If anything covers any of these slots then the injection fails.
-        /// </summary>
-        [DataField("blockSlots"), ViewVariables(VVAccess.ReadWrite)]
-        public SlotFlags BlockSlots = SlotFlags.MASK;
-    }
+    [DataField("blockSlots"), ViewVariables(VVAccess.ReadWrite)]
+    public SlotFlags BlockSlots = SlotFlags.MASK;
 }

Content.Server/Chemistry/EntitySystems/SolutionInjectOnCollideSystem.cs
@@ -6,48 +6,47 @@ using Content.Shared.Inventory;
 using JetBrains.Annotations;
 using Robust.Shared.Physics.Events;
 
-namespace Content.Server.Chemistry.EntitySystems
+namespace Content.Server.Chemistry.EntitySystems;
+
+public sealed class SolutionInjectOnCollideSystem : EntitySystem
 {
-    [UsedImplicitly]
-    internal sealed class SolutionInjectOnCollideSystem : EntitySystem
+    [Dependency] private readonly SolutionContainerSystem _solutionContainersSystem = default!;
+    [Dependency] private readonly BloodstreamSystem _bloodstreamSystem = default!;
+    [Dependency] private readonly InventorySystem _inventorySystem = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly SolutionContainerSystem _solutionContainersSystem = default!;
-        [Dependency] private readonly BloodstreamSystem _bloodstreamSystem = default!;
-        [Dependency] private readonly InventorySystem _inventorySystem = default!;
+        base.Initialize();
+        SubscribeLocalEvent<SolutionInjectOnCollideComponent, StartCollideEvent>(HandleInjection);
+    }
 
-        public override void Initialize()
+    private void HandleInjection(Entity<SolutionInjectOnCollideComponent> ent, ref StartCollideEvent args)
+    {
+        var component = ent.Comp;
+        var target = args.OtherEntity;
+
+        if (!args.OtherBody.Hard ||
+            !args.OurBody.Hard ||
+            !EntityManager.TryGetComponent<BloodstreamComponent>(target, out var bloodstream) ||
+            !_solutionContainersSystem.TryGetInjectableSolution(ent.Owner, out var solution, out _))
         {
-            base.Initialize();
-            SubscribeLocalEvent<SolutionInjectOnCollideComponent, StartCollideEvent>(HandleInjection);
+            return;
         }
 
-        private void HandleInjection(Entity<SolutionInjectOnCollideComponent> ent, ref StartCollideEvent args)
+        if (component.BlockSlots != 0x0)
         {
-            var component = ent.Comp;
-            var target = args.OtherEntity;
+            var containerEnumerator = _inventorySystem.GetSlotEnumerator(target, component.BlockSlots);
 
-            if (!args.OtherBody.Hard ||
-                !EntityManager.TryGetComponent<BloodstreamComponent>(target, out var bloodstream) ||
-                !_solutionContainersSystem.TryGetInjectableSolution(ent.Owner, out var solution, out _))
-            {
+            // TODO add a helper method for this?
+            if (containerEnumerator.MoveNext(out _))
                 return;
-            }
-
-            if (component.BlockSlots != 0x0)
-            {
-                var containerEnumerator = _inventorySystem.GetSlotEnumerator(target, component.BlockSlots);
-
-                // TODO add a helper method for this?
-                if (containerEnumerator.MoveNext(out _))
-                    return;
-            }
-
-            var solRemoved = _solutionContainersSystem.SplitSolution(solution.Value, component.TransferAmount);
-            var solRemovedVol = solRemoved.Volume;
-
-            var solToInject = solRemoved.SplitSolution(solRemovedVol * component.TransferEfficiency);
-
-            _bloodstreamSystem.TryAddToChemicals(target, solToInject, bloodstream);
         }
+
+        var solRemoved = _solutionContainersSystem.SplitSolution(solution.Value, component.TransferAmount);
+        var solRemovedVol = solRemoved.Volume;
+
+        var solToInject = solRemoved.SplitSolution(solRemovedVol * component.TransferEfficiency);
+
+        _bloodstreamSystem.TryAddToChemicals(target, solToInject, bloodstream);
     }
 }

AdventureSpaceTeam/AdventureSpace

commit 286967d2af96a1bae4b5868f66d724259ae2010b

Fix tranquiliser shells not working (#23886)
* Fix tranquiliser darts not working

Fixes a bug introduced by 9f47079d02a872f0aaff84f4556b83d3c6f835ef which
was made to stop the fly-by fixture from triggering the system. This was
done by checking whether the fixture was hard. Apparently the
projectile's fixture is never hard as well. The change just makes it so
that check only succeeds when the fixture is a fly-by fixture.

* Remove something that I think is redundant

* Remove random using directive that somehow appeared.

* Address Review

* Adress Review 2

* Put the appropriate fixture ids

Content.Server/Chemistry/Components/SolutionInjectOnCollideComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Content.Shared.FixedPoint;
 using Content.Shared.Inventory;
+using Content.Shared.Projectiles;
 
 namespace Content.Server.Chemistry.Components;
 
@@ -24,4 +25,7 @@ public sealed partial class SolutionInjectOnCollideComponent : Component
     /// </summary>
     [DataField("blockSlots"), ViewVariables(VVAccess.ReadWrite)]
     public SlotFlags BlockSlots = SlotFlags.MASK;
+
+    [DataField]
+    public string FixtureId = SharedProjectileSystem.ProjectileFixture;
 }

Content.Server/Chemistry/EntitySystems/SolutionInjectOnCollideSystem.cs
@@ -26,7 +26,7 @@ public sealed class SolutionInjectOnCollideSystem : EntitySystem
         var target = args.OtherEntity;
 
         if (!args.OtherBody.Hard ||
-            !args.OurBody.Hard ||
+            args.OurFixtureId != ent.Comp.FixtureId ||
             !EntityManager.TryGetComponent<BloodstreamComponent>(target, out var bloodstream) ||
             !_solutionContainersSystem.TryGetInjectableSolution(ent.Owner, out var solution, out _))
         {

Resources/Prototypes/Entities/Objects/Fun/darts.yml
@@ -55,6 +55,7 @@
   - type: SolutionInjectOnCollide
     transferAmount: 2
     blockSlots: OUTERCLOTHING
+    fixtureId: "throw-fixture"
   - type: SolutionTransfer
     maxTransferAmount: 2
   - type: Damageable
@@ -125,6 +126,7 @@
   - type: SolutionInjectOnCollide
     transferAmount: 7
     blockSlots: NONE
+    fixtureId: "throw-fixture"
   - type: SolutionTransfer
     maxTransferAmount: 7
 

Resources/Prototypes/Entities/Objects/Weapons/Melee/spear.yml
@@ -67,6 +67,7 @@
     solution: melee
   - type: SolutionInjectOnCollide
     transferAmount: 2
+    fixtureId: "throw-fixture"
     blockSlots: NONE
   - type: SolutionTransfer
     maxTransferAmount: 2