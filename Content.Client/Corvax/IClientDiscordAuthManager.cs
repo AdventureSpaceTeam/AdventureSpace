5ab72de77a9d8669762d2cf460dc409b9d3f7018
VigersRay <vigersray@gmail.com>
2024-03-01 01:41:13 +0200

    Более красивые окна дискорд авторизации. Предупреждения про подвязку. Указание юзернейма текущей подвязки.

--- Content.Client/Corvax/IClientDiscordAuthManager.cs
+++ Content.Client/Corvax/IClientDiscordAuthManager.cs
@@ -9,4 +9,5 @@ public interface IClientDiscordAuthManager : ISharedDiscordAuthManager
     public Texture? Qrcode { get; }
     public string DiscordUrl { get; }
     public Texture? DiscordQrcode { get; }
+    public string DiscordUsername { get; }
 }
