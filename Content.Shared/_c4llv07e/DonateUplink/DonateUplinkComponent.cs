30a53c6a79595d47237f49995a82b3a214bf4b44
c4llv07e <kseandi@gmail.com>
2024-03-09 20:54:26 +0300

    disassembly donate uplink
    Signed-off-by: c4llv07e <kseandi@gmail.com>

---
+++ Content.Shared/_c4llv07e/DonateUplink/DonateUplinkComponent.cs
@@ -0,0 +1,36 @@
+﻿using Content.Shared.DoAfter;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._c4llv07e.DonateUplink;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class DonateUplinkComponent : Component
+{
+    [DataField]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public bool Opened = false;
+
+    [DataField]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string BackplateText = "Спасибо за донат!";
+}
+
+[Serializable, NetSerializable]
+public sealed class DonateUplinkComponentState : ComponentState
+{
+    public bool Open { get; init; }
+}
+
+
+[Serializable, NetSerializable]
+public sealed partial class DonateUplinkScrewingFinishedEvent : SimpleDoAfterEvent
+{
+}
+
+[Serializable, NetSerializable]
+public enum DonateUplinkVisualLayers : byte
+{
+    Base,
+    Opened,
+}
