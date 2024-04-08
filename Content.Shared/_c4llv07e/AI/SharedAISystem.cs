f762be36b9e8812b6124ee6dacd4077d521aa0d8
c4llv07e <igor@c4llv07e.xyz>
2024-04-08 22:57:15 +0300

    Свободу ИИ от коробок! (ИИ теперь нельзя запереть в коробке)
    Signed-off-by: c4llv07e <igor@c4llv07e.xyz>

--- Content.Shared/_c4llv07e/AI/SharedAISystem.cs
+++ Content.Shared/_c4llv07e/AI/SharedAISystem.cs
@@ -1,7 +1,8 @@
+using Content.Shared.Hands.Components;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Interaction.Components;
 using Content.Shared.Interaction.Events;
-using Content.Shared.Hands.Components;
+using Content.Shared.Storage.Components;
 using Content.Shared._c4llv07e.AI;
 
 namespace Content.Shared._c4llv07e.AI;
@@ -11,11 +12,17 @@ public sealed class SharedAISystem : EntitySystem
     public override void Initialize()
     {
         SubscribeLocalEvent<AIComponent, InteractionAttemptEvent>(OnInteract);
+        SubscribeLocalEvent<AIComponent, InsertIntoEntityStorageAttemptEvent>(OnEntityStorageInsertAttempt);
     }
 
     public void OnInteract(Entity<AIComponent> ent, ref InteractionAttemptEvent args)
     {
         if (!HasComp<AIControlledComponent>(args.Target) && args.Target != null)
             args.Cancel();
+    }
+
+    private void OnEntityStorageInsertAttempt(Entity<AIComponent> ent, ref InsertIntoEntityStorageAttemptEvent args)
+    {
+        args.Cancelled = true;
     }
 }
