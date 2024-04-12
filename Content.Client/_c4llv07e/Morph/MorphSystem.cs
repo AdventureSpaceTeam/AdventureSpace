31c453cc53ccd7f4b92c468b31ea6920e8b6115a
c4llv07e <igor@c4llv07e.xyz>
2024-04-12 23:51:34 +0300

    Add ability to the morph to use the original sprite

--- Content.Client/_c4llv07e/Morph/MorphSystem.cs
+++ Content.Client/_c4llv07e/Morph/MorphSystem.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Content.Shared.Verbs;
 using Content.Shared._c4llv07e.Morph;
 using Robust.Client.GameObjects;
@@ -33,33 +34,53 @@ public sealed class SharedMorphSystem : EntitySystem
         var i = 0;
         foreach (var layer in args.Sprite.AllLayers)
         {
-            args.Sprite.LayerSetSprite(i, spriteSpecifier);
+            // I think less errors is always better :/
+            // Just for clarification, Mob entity has two or more layers:
+            // the actual entity sprites and a speech buble. The game
+            // doesn't like when we touch speech's rsi so we check if the
+            // layer is buble before we change it.
+            // I can't think a better way to check it, ISpriteLayer is too
+            // empty
+            if (layer.Rsi?.Path.CanonPath != "/Textures/Effects/speech.rsi")
+                args.Sprite.LayerSetSprite(i, spriteSpecifier);
             i += 1;
         }
+    }
+    private bool GetEntitySprite(EntityUid target, [NotNullWhen(true)] out ResPath? path, [NotNullWhen(true)] out string? state)
+    {
+        path = null;
+        state = null;
+        if (!TryComp(target, out SpriteComponent? sprite) || !sprite.Visible)
+            return false;
+        if (sprite.BaseRSI == null)
+            return false;
+        path = sprite.BaseRSI.Path;
+        var rsiState = sprite.LayerGetState(0);
+        if (rsiState == null)
+            return false;
+        if (rsiState.Name == null)
+            return false;
+        state = rsiState.Name;
+        return true;
     }
     private void AddMorphVerb(Entity<MorphComponent> ent, ref GetVerbsEvent<InnateVerb> args)
     {
         var user = args.User;
         var target = args.Target;
-        if (!TryComp(target, out SpriteComponent? sprite) || !sprite.Visible)
+        ResPath? path = new ResPath("/Textures/_c4llv07e/morph.rsi");
+        string? state = "morph";
+        if (target != user && !GetEntitySprite(target, out path, out state))
             return;
-        if (sprite.BaseRSI == null)
-            return;
-        var rsiPath = sprite.BaseRSI.Path;
-        var rsiState = sprite.LayerGetState(0);
-        if (rsiState == null)
-            return;
-        if (rsiState.Name == null)
-            return;
+
         InnateVerb verb = new()
         {
             Act = () =>
             {
-                var ev = new MorphEvent(GetNetEntity(user), rsiPath, rsiState.Name);
+                var ev = new MorphEvent(GetNetEntity(user), path.Value, state);
                 RaiseNetworkEvent(ev);
             },
             Text = "Замоскироваться под предмет",
-            Icon = new SpriteSpecifier.Rsi(rsiPath, rsiState.Name),
+            Icon = new SpriteSpecifier.Rsi(path.Value, state),
             Priority = 2
         };
         args.Verbs.Add(verb);
