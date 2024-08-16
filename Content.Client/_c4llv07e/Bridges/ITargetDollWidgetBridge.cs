f001e7c09f298f29b7d68b12fb862d1900d321e5
darkeneez <darkeneez@gmail.com>
2024-08-16 12:23:18 +0500

    Surgery, doll bridge + vulpa tail

---
+++ Content.Client/_c4llv07e/Bridges/ITargetDollWidgetBridge.cs
@@ -0,0 +1,43 @@
+﻿using Content.Shared.Body.Part;
+using Robust.Client.UserInterface;
+
+namespace Content.Client._c4llv07e.Bridges;
+
+public interface ITargetDollWidgetBridge
+{
+    public void SelectBodyPart(BodyPartType? targetBodyPart, BodyPartSymmetry bodyPartSymmetry);
+    public void Clear();
+    public void Hide();
+    public void Show();
+    public void InitializeWidget();
+    public void SetupWidget(Control surface);
+}
+
+public sealed class StubTargetDollWidgetBridge : ITargetDollWidgetBridge
+{
+    public void SelectBodyPart(BodyPartType? targetBodyPart, BodyPartSymmetry bodyPartSymmetry)
+    {
+    }
+
+    public void Clear()
+    {
+
+    }
+
+    public void Hide()
+    {
+    }
+
+    public void Show()
+    {
+    }
+
+    public void InitializeWidget()
+    {
+    }
+
+    public void SetupWidget(Control surface)
+    {
+
+    }
+}
